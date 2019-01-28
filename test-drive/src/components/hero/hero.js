e1077647bfc726816727c92c203c5fbf972bb21b
Phil Ramirez <ramirezp6856@gmail.com>
2019-01-27 22:01:20 -0500

    Update Hero component and test drive css

--- test-drive/src/components/hero/hero.js
+++ test-drive/src/components/hero/hero.js
@@ -2,13 +2,66 @@ import React from "react";
 import { StaticQuery, graphql } from "gatsby";
 import styled from "styled-components";
 
+/*
+ * Source: https://codepen.io/pmalexis/pen/omvwEm
+ */
+
 const HeroDiv = styled.div`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid palevioletred;
-  color: palevioletred;
-  margin: 0.5em 1em;
-  padding: 0.25em 1em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`;
+
+const HeroH1 = styled.h1`
+  position: relative;
+  font-family: "Roboto", sans-serif;
+  font-size: 75px;
+
+  &:after,
+  &:before {
+    content: " ";
+    position: absolute;
+    z-index: -1;
+    height: 100px;
+    width: 100px;
+    transition-duration: 0.7s;
+    border: 0 solid #272822;
+    opacity: 0.7;
+  }
+
+  &:before {
+    border-right-width: 10px;
+    border-top-width: 10px;
+    border-right-color: #000;
+    border-top-color: #000;
+    right: -35px;
+    top: -25px;
+  }
+
+  &:after {
+    border-bottom-width: 10px;
+    border-left-width: 10px;
+    bottom: -25px;
+    left: -35px;
+  }
+
+  &:hover:before,
+  &:hover:after {
+    width: 100vw;
+    height: 100vh;
+    opacity: 1;
+  }
+`;
+
+const HeroA = styled.a`
+  text-decoration: none;
+  color: #000;
+
+  &:hover {
+    color: #9036d4;
+  }
 `;
 
 const Hero = () => (
@@ -22,7 +75,15 @@ const Hero = () => (
         }
       }
     `}
-    render={data => <HeroDiv>{data.site.siteMetadata.name}</HeroDiv>}
+    render={data => (
+      <HeroDiv>
+        <HeroH1>
+          <HeroA href="https://www.youtube.com/watch?v=D-UmfqFjpl0" target="_blank">
+            {data.site.siteMetadata.name}
+          </HeroA>
+        </HeroH1>
+      </HeroDiv>
+    )}
   />
 );
 
