fbfd024b9189f8dc020a388655a8b05e91427c3c
Phillip Ramirez <ramirezp6856@gmail.com>
2019-02-21 14:00:05 -0500

    See below...
    * Refactor header and logo components
    * Add logo favicon
    * Add stock images

--- test-drive/src/components/header/header.js
+++ test-drive/src/components/header/header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { StaticQuery, graphql } from "gatsby";
 import styled from "styled-components";
+import Logo from "../logo/logo"
 import PropTypes from "prop-types";
 
 // Sources:
@@ -49,42 +50,12 @@ const NavItem = styled.li`
   margin-right: 60px;
 `;
 
-const LogoWrapper = styled.div`
-  height: 30px;
-  width: 23px;
-`;
-
-const LogoSvg = styled.svg`
-  width: 100%;
-  height: 100%;
-  fill: none;
-`;
-
-// Read more: https://blog.lftechnology.com/using-svg-icons-components-in-react-44fbe8e5f91
-const LogoPath = styled.path`
-  stroke: #1b2b34;
-  stroke-width: 1px;
-  fill: #d8dee9;
-`;
-
 const Header = ({ data }) => {
   return (
     <Container>
       <Wrapper>
         <Kimono>
-          {/* TODO Change to LINK tag*/}
-          <a href="/">
-            <LogoWrapper>
-              <LogoSvg xmlns="http://www.w3.org/2000/svg">
-                {/* _ */}
-                <LogoPath d="M0 16H17.9091V20.4595H0V30Z" />
-                {/* | */}
-                <LogoPath d="M0.00598145 30.7176L7.01982 30.7873L7.01897 12.2965L0.00598145 10.3745V24.7176Z" />
-                {/* \ */}
-                <LogoPath d="M 0.0065918 0 V 8.62637 L 8.8961 16.7297 L 18.8948 17.0316 L 0.0065918 0 Z" />
-              </LogoSvg>
-            </LogoWrapper>
-          </a>
+          <Logo />
           <Wayfinder>
             <NavList>{_getNavItems(data)}</NavList>
           </Wayfinder>
---
+++ test-drive/src/components/logo/logo.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "gatsby";
+
+const LogoWrapper = styled.div`
+  height: 30px;
+  width: 23px;
+`;
+
+const LogoSvg = styled.svg`
+  width: 100%;
+  height: 100%;
+  fill: none;
+`;
+
+// Read more: https://blog.lftechnology.com/using-svg-icons-components-in-react-44fbe8e5f91
+const LogoPath = styled.path`
+  stroke: #1b2b34;
+  stroke-width: 1px;
+  fill: #d8dee9;
+`;
+
+export default class Logo extends React.Component {
+  render() {
+    return (
+      <Link to="/">
+        <LogoWrapper>
+          <LogoSvg xmlns="http://www.w3.org/2000/svg">
+            {/* _ */}
+            <LogoPath d="M0 17.5H18.2091V21.2595H0V30Z" />
+            {/* | */}
+            <LogoPath d="M0.00598145 30.7176L7.01982 30.7873L7.01897 12.2965L0.00598145 10.3745V24.7176Z" />
+            {/* \ */}
+            <LogoPath d="M 0.0065918 0 V 9.62637 L 18.8961 20.6297 L 18.5948 17.50316 L 0.0065918 0 Z" />
+          </LogoSvg>
+        </LogoWrapper>
+      </Link>
+    );
+  }
+}
